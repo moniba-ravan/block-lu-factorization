dc988cf2cffa20e901c51b5c3ef942c7fba13ebb
Moniba <moniba.ravan@gmail.com>
2024-08-04 22:45:36 +0330

    remoce some prints

--- test.py
+++ test.py
@@ -17,8 +17,6 @@ def extraxt_LU_from_compact(A):
 
 
 def check_matrix(A, B):
-    print(A)
-    print(B)
     
     if np.allclose(A, B):
         # if two matrix are almost equal!
@@ -27,7 +25,7 @@ def check_matrix(A, B):
         print(f"NOT Equal!")
 
 if __name__ == "__main__":
-  
+    verbose = False
     if len(sys.argv) != 3:
         print("Usage: python test.py N block_size")
         sys.exit(1)
@@ -44,39 +42,38 @@ if __name__ == "__main__":
         for j in range(N):
             A[i][j] = random.randint(1, 100)
 
-    A = np.array([[1, 2, 3, 4, 5, 6, 7, 8], 
-                  [4, 3, 2, 4, 2, 2, 2, 1],
-                  [2, 2, 1, 4, 5, 1, 2, 2],
-                  [4, 2, 1, 5, 1, 1, 1, 1],
-                  [3, 3, 3, 3, 6, 5, 5, 5],
-                  [2, 2, 2, 2, 4, 4, 4, 2],
-                  [3, 2, 1, 4, 3, 2, 1, 4],
-                  [1, 5, 6, 7, 8, 5, 4, 3]], dtype=float)
+    if verbose:
+        A = np.array([[1, 2, 3, 4, 5, 6, 7, 8], 
+                    [4, 3, 2, 4, 2, 2, 2, 1],
+                    [2, 2, 1, 4, 5, 1, 2, 2],
+                    [4, 2, 1, 5, 1, 1, 1, 1],
+                    [3, 3, 3, 3, 6, 5, 5, 5],
+                    [2, 2, 2, 2, 4, 4, 4, 2],
+                    [3, 2, 1, 4, 3, 2, 1, 4],
+                    [1, 5, 6, 7, 8, 5, 4, 3]], dtype=float)
 
     # Keep the origin matrix A
     origin_A = A.copy()
-    print("Original matrix A:")
-    print(origin_A)
+    if verbose:
+        print("Original matrix A:")
+        print(origin_A)
 
     # Calculate
     start_time = time.time()
-    ## My solution
     block_lu(N, block_size, A)
-
-    ## build-in func
-    # sp.linalg.lu(A, overwrite_a=True)
     end_time = time.time()
-
-    print("\nLU-decomposed matrix A:")
-    print(A)
+    if verbose:
+        print("\nLU-decomposed matrix A:")
+        print(A)
     
 
     # Evaluate the solution
     # Correctness
     L, U = extraxt_LU_from_compact(A)
     made_A = L @ U
-    print("Made A:")
-    print(made_A)
+    if verbose:
+        print("Made A:")
+        print(made_A)
     check_matrix(origin_A, made_A)
 
     # Solution Runtime 
